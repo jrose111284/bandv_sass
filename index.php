96493cde605680376b820e38f7b04b1df85f6ea7
jamierose <jrose111284@gmail.com>
2016-05-16 10:55:20 +0100

    tidy up

--- index.php
+++ index.php
@@ -140,13 +140,8 @@
       </div>
   </div>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script>
-    window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')
-  </script>
-  <script src="../../dist/js/bootstrap.min.js"></script>
-
-  <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="js/bootstrap.js"></script>
 
 
 </body>
